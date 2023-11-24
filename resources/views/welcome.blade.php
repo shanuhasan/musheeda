3501df5bb2d6a1113e5e4ed0a0635ce0fd52e561
Shahnawaz <shahnawaz@ranoliaventures.com>
2023-11-24 14:14:03 +0530

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -864,11 +864,11 @@
                             Log
                             in</a>
 
-                        @if (Route::has('admin.register'))
+                        {{-- @if (Route::has('admin.register'))
                             <a href="{{ route('admin.register') }}"
                                 class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Admin
                                 Register</a>
-                        @endif
+                        @endif --}}
                     @endauth
                 </div>
             @endif
