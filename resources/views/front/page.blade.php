f19620c9de2935f9bbdbc66773218fed3af7ec58
Shahnawaz <shahnawaz@ranoliaventures.com>
2023-11-24 18:45:09 +0530

    updates

--- resources/views/front/page.blade.php
+++ resources/views/front/page.blade.php
@@ -2,83 +2,66 @@
 @section('title', $page->name)
 @section($page->name, 'active')
 @section('content')
-    <section class="section-5 pt-3 pb-3 mb-3 bg-white">
-        <div class="container">
-            <div class="light-font">
-                <ol class="breadcrumb primary-color mb-0">
-                    <li class="breadcrumb-item"><a class="white-text" href="{{ route('home.index') }}">Home</a></li>
-                    <li class="breadcrumb-item">{{ $page->name }}</li>
-                </ol>
-            </div>
-        </div>
-    </section>
 
     @if ($page->slug == 'contact-us')
-        <section class=" section-10">
+        <div class="contact mt-70">
             <div class="container">
-                <div class="section-title mt-5 ">
+                <div class="section-header">
                     <h2>{{ $page->name }}</h2>
                 </div>
-            </div>
-        </section>
 
-        <section>
-            <div class="container">
                 <div class="row">
-                    @include('admin.message')
-                    <div class="col-md-6 mt-3 pe-lg-5">
-                        {!! $page->content !!}
-                    </div>
-
                     <div class="col-md-6">
-                        <form class="shake" role="form" method="post" id="contactForm" name="contactForm">
-                            @csrf
-                            <div class="mb-3">
-                                <label class="mb-2" for="name">Name</label>
-                                <input class="form-control" id="name" type="text" name="name"
-                                    data-error="Please enter your name">
-                                <p class="help-block with-errors"></p>
-                            </div>
-
-                            <div class="mb-3">
-                                <label class="mb-2" for="email">Email</label>
-                                <input class="form-control" id="email" type="email" name="email"
-                                    data-error="Please enter your Email">
-                                <p class="help-block with-errors"></p>
-                            </div>
-
-                            <div class="mb-3">
-                                <label class="mb-2">Subject</label>
-                                <input class="form-control" id="subject" type="text" name="subject"
-                                    data-error="Please enter your message subject">
-                                <p class="help-block with-errors"></p>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="message" class="mb-2">Message</label>
-                                <textarea class="form-control" rows="3" id="message" name="message" data-error="Write your message"></textarea>
-                                <p class="help-block with-errors"></p>
-                            </div>
-
-                            <div class="form-submit">
-                                <button class="btn btn-dark" type="submit" id="form-submit"><i
-                                        class="material-icons mdi mdi-message-outline"></i> Send Message</button>
-                                <div id="msgSubmit" class="h3 text-center hidden"></div>
-                                <div class="clearfix"></div>
-                            </div>
-                        </form>
+                        <div class="form">
+                            <form class="contactForm">
+                                <div class="form-row">
+                                    <div class="form-group col-sm-6">
+                                        <input type="text" class="form-control" placeholder="Your Name" />
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                        <input type="email" class="form-control" placeholder="Your Email" />
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Subject" />
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control" rows="5" placeholder="Message"></textarea>
+                                </div>
+                                <div><button class="btn" type="submit">Send Message</button></div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="map">
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26361250.667320687!2d-113.75533773453304!3d36.24128894212527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sbd!4v1574923227698!5m2!1sen!2sbd"
+                                frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     @else
-        <section class=" section-10">
+        <div class="single mt-70">
             <div class="container">
-                <h1 class="my-3">{{ $page->name }}</h1>
-                {!! $page->content !!}
+                <div class="section-header">
+                    <h2>{{ $page->name }}</h2>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-md-12">
+                        <div class="single-img">
+                            <img src="{{ asset('front-assets/img/single.jpg') }}" alt="" class="img-fluid">
+                        </div>
+                        <div class="single-content">
+                            {!! $page->content !!}
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
+        </div>
     @endif
+
 
 @endsection
 
