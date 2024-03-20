dd1db5fe995277025999c309b6b9457d7a63d421
Shahnawaz <shahnawaz@ranoliaventures.com>
2024-03-20 16:56:29 +0530

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,6 @@
     {{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
     <script src="{{ asset('front-assets/js/google-map.js') }}"></script> --}}
     <script src="{{ asset('front-assets/js/main.js') }}"></script>
-    <script src="{{ asset('front-assets/js/contact-us.js') }}"></script>
 
     <script>
         $.ajaxSetup({
